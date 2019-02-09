3d01d3d715bdb85d92ab02234c7513ba04ae989a
Alden Chen <alden.chen@outlook.com>
2019-02-09 07:03:11 -0800

    remove tests

--- src/crop.py
+++ src/crop.py
@@ -8,7 +8,7 @@ def crop(image, height, width):
     Function to crop images
 
     Parameters:
-    image, a 3d array of the image (obtained using plt.imread(image))
+    image, a 3d array of the image (can be obtained using plt.imread(image))
     height, integer, the desired height of the cropped image
     width, integer, the desired width of the cropped image
 
@@ -20,52 +20,3 @@ def crop(image, height, width):
     '''
     pass
 
-def crop_test(image):
-    '''
-    Function to test crop() function
-
-    Parameter: image, a 3d array of an image to be used to test crop
-    Returns: None
-
-    '''
-
-    # Test for correct shape
-    assert crop(image, 15, 10).shape == (15, 10, 3)
-    assert crop(image, 10, 5).shape == (10, 4, 3)
-    assert crop(image, 6, 6).shape == (6, 6, 3)
-    assert crop(image, 1, 1).shape == (1, 1, 3)
-    assert crop(image, image.shape[0], image.shape[1]).shape == image.shape
-
-    # ValueError should be raised when invalid dimensions are entered
-    with pytest.raises(ValueError):
-        crop(image, -1, 10)
-    with pytest.raises(ValueError):
-        crop(image, 10, -1)
-    with pytest.raises(ValueError):
-        crop(image, 0, 0)
-    with pytest.raises(ValueError):
-        crop(image, 0, 10)
-    with pytest.raises(ValueError):
-        crop(image, 10, 0)
-    with pytest.raises(ValueError):
-        crop(image, int(1e30), 10)
-    with pytest.raises(ValueError):
-        crop(image, 10, int(1e30))
-    with pytest.raises(ValueError):
-        crop(image, int(-1e30), 10)
-    with pytest.raises(ValueError):
-        crop(image, 10, int(-1e30))
-
-    # TypeError should be raised when the wrong type is entered
-    with pytest.raises(TypeError):
-        crop(image, 9.5, 10)
-    with pytest.raises(TypeError):
-        crop(image, 10, 9.5)
-    with pytest.raises(TypeError):
-        crop(image, 9.5, 9.5)
-    with pytest.raises(TypeError):
-        crop(image, -9.9, -4.5)
-    with pytest.raises(TypeError):
-        crop(image, "10", True)
-    with pytest.raises(TypeError):
-        crop("image", 10, 10)
--- src/image_size.py
+++ src/image_size.py
@@ -16,28 +16,3 @@ def image_size(image):
     """
 
     pass
-
-def image_size_test(image):
-    """
-    Function to test image_size()
-
-    Parameters: image, a 3d array representation of an image
-    like the on returned by plt.imread()
-
-    Returns: none
-
-    Example:
-    image_size_test(image)
-    """
-
-    # test that size returned is not too big
-    assert image_size(image, b) < (b+1) * np.prod(image.shape)/8 
-
-    # TypeError should be raised when wrong type passed in
-    with pytest.raises(TypeError):
-        image_size("file/path/to/image.jpg/or/image.png", b)
-    with pytest.raises(TypeError):
-
-
-    # 
-
