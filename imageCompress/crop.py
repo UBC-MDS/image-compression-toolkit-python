7598bae4739c4f98a9b6c8055f1b6f04b0952bde
Aditya Sharma <adityashrm21@gmail.com>
2019-02-13 17:42:14 -0800

    adding the compress and image_size function with new dir str

--- imageCompress/crop.py
+++ imageCompress/crop.py
@@ -1,18 +1,18 @@
-
 def crop(image, height, width):
-    '''
+    """
     Function to crop images
 
     Parameters:
-    image, a 3d array of the image (can be obtained using plt.imread(image))
+    ----------
+    image, a 3d array of the image (obtained using plt.imread(image))
     height, integer, the desired height of the cropped image
     width, integer, the desired width of the cropped image
 
     Returns:
+    --------
     cropped_image, a 3d array with dimensions height x width x 3
 
     Example:
     crop(image, 10, 15) returns an array with shape (10, 15, 3)
-    '''
+    """
     pass
-
