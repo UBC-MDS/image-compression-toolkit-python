c9b5ac89cfa1f7c549d2d27b09a1adadd2ba4be0
Aditya Sharma <adityashrm21@gmail.com>
2019-02-15 12:38:57 -0800

    correcting url

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ setup(
     packages=['imageCompress'],
     scripts=['imageCompress/compress.py', 'imageCompress/crop.py', 'imageCompress/image_size.py'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    url='https://github.com/UBC-MDS/image-compression-toolkit-python.git',
+    url='https://github.com/UBC-MDS/image-compression-toolkit-python',
+    download_url='https://github.com/UBC-MDS/image-compression-toolkit-python',
     long_description='open("README.md").read()',
     install_requires=['scikit-image', 'numpy', 'scikit-learn', 'pytest'],
     author='Aditya Sharma | Sayanti Ghosh | Alden Chen'
