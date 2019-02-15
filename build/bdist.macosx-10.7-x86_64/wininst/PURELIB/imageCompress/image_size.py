e01d60e9cda54d8059f3f9286c5ec945c6c41724
Aditya Sharma <adityashrm21@gmail.com>
2019-02-15 12:33:12 -0800

    adding more info to setup.py and correcting previous ones

---
+++ build/bdist.macosx-10.7-x86_64/wininst/PURELIB/imageCompress/image_size.py
@@ -0,0 +1,22 @@
+import os
+
+def image_size(img_path):
+    """
+    Function to estimate the size of an image in bytes
+
+    Parameters:
+    ----------
+    img_path : str (file path to the image)
+
+    Returns:
+    --------
+    size, integer, estimated size of the image in number of bytes
+
+    Example:
+    image_size(image_path)
+    """
+    if type(img_path) != str:
+        raise TypeError("Image path invalid, pass a correct path (string)!")
+        
+    file_info = os.stat(img_path)
+    return file_info.st_size
--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@ from distutils.core import setup
 
 setup(
     name='imageCompress',
-    version='0.1dev',
+    version='0.2dev',
     packages=['imageCompress'],
-    scripts=['imageCompress/util.py'],
+    scripts=['imageCompress/compress.py', 'imageCompress/crop.py', 'imageCompress/image_size.py'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    url='https://github.com/UBC-MDS/image-compression-toolkit-python'
+    url='https://github.com/UBC-MDS/image-compression-toolkit-python.git',
+    long_description='open("README.md").read()',
+    install_requires=['skimage', 'numpy', 'scikit-learn', 'pytest'],
+    author='Aditya Sharma | Sayanti Ghosh | Alden Chen'
 )
