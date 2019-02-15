964c3300e72412f3a9db895afa16041638184a9a
Aditya Sharma <adityashrm21@gmail.com>
2019-02-15 12:36:53 -0800

    adding correct package name for skimage

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     url='https://github.com/UBC-MDS/image-compression-toolkit-python.git',
     long_description='open("README.md").read()',
-    install_requires=['skimage', 'numpy', 'scikit-learn', 'pytest'],
+    install_requires=['scikit-image', 'numpy', 'scikit-learn', 'pytest'],
     author='Aditya Sharma | Sayanti Ghosh | Alden Chen'
 )
